9fb5f1529e7ed9b05953b07df160822210765d1f
Sean Goff <sean318@hotmail.ch>
2021-07-09 02:53:40 +0200

    WIP

--- docker/pack.js
+++ docker/pack.js
@@ -1,10 +1,11 @@
 const { execSync } = require('child_process');
-const { writeFileSync } = require('fs');
+const { writeFileSync, mkdirSync } = require('fs');
+const path = require('path/posix');
 
 const lernaListResult = execSync('npx lerna list --scope directus --include-dependencies --json');
 
 const list = JSON.parse(String(lernaListResult));
-const apiPackageJSON = require('./api/package.json');
+const apiPackageJSON = require(path.resolve(__dirname, '../api/package.json'));
 
 const packageJSON = {
 	name: 'directus-project',
@@ -13,9 +14,13 @@ const packageJSON = {
 	dependencies: apiPackageJSON.optionalDependencies,
 };
 
+mkdirSync('dist');
+
 list.forEach((package) => {
-	const tarName = String(execSync(`npm pack ${package.location}`)).trim();
+	const tarName = String(
+		execSync(`npm pack ${package.location}`, { cwd: path.resolve(__dirname, '..', 'dist') })
+	).trim();
 	packageJSON.dependencies[package.name] = `file:${tarName}`;
 });
 
-writeFileSync('./package.json', JSON.stringify(packageJSON, null, 4));
+writeFileSync(path.resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJSON, null, 4));
